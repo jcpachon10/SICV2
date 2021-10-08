3f924c7a7b9749809d25dd2784045eec5acbe244
jcpachon10 <jc.pachon10@uniandes.edu.co>
2021-10-08 10:47:15 -0500

    Primer envio

---
+++ Assets/Scripts/GlobalLanguage.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GlobalLanguage : MonoBehaviour
+{
+    public static string currentLanguage = "Español";
+    public Toggle toggleEs;
+    public Toggle toggleEn;
+    public bool MenuScene;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+       // DontDestroyOnLoad(transform.gameObject);
+    }
+
+    private void Start()
+    {
+        changeLanguage(currentLanguage);
+    }
+    public string returnLanguage()
+    {
+        return (currentLanguage);
+    }
+     public void changeLanguage(string language1)
+    {
+        print(language1);
+        currentLanguage = language1;
+        NotificationCenter.DefaultCenter().PostNotification(this,"ChangeLanguage_");
+        if (MenuScene)
+        {
+            if (language1 == "Español")
+            {
+                toggleEs.isOn = true;
+            }
+            else if (language1 == "English")
+            {
+                toggleEn.isOn = true;
+            }
+        }
+       
+    }
+}
